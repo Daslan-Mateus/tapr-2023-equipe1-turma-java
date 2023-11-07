86040b3852b70feb5df7f2bfb8b0a347ff2b348d
Daslan Mateus <70038813+Daslan-Mateus@users.noreply.github.com>
2023-11-07 22:56:33 +0000

    continuando com controller

--- microservturma/src/main/java/br/edu/univille/microservturma/controller/TurmaAPIController.java
+++ microservturma/src/main/java/br/edu/univille/microservturma/controller/TurmaAPIController.java
@@ -1,11 +1,20 @@
 package br.edu.univille.microservturma.controller;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import static org.springframework.http.HttpStatus;
+
+import br.edu.univille.microservturma.entity.Turma;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 
 @Controller 
 @RequestMapping("api/v1/turma")
 public class TurmaAPIController {
+    @GetMapping
     public ResponseEntity<List<Turma>> listaTurma (){
         var  listaTurma = new ArrayList<Turma>();
 
