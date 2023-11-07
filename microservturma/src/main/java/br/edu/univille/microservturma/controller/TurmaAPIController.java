423b105287f253aa4ea9982710a339f0a350ee05
Daslan Mateus <70038813+Daslan-Mateus@users.noreply.github.com>
2023-11-07 21:56:12 +0000

    controller

--- microservturma/src/main/java/br/edu/univille/microservturma/controller/TurmaAPIController.java
+++ microservturma/src/main/java/br/edu/univille/microservturma/controller/TurmaAPIController.java
@@ -1,10 +1,16 @@
 package br.edu.univille.microservturma.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import static org.springframework.http.HttpStatus;
 
 @Controller 
 @RequestMapping("api/v1/turma")
 public class TurmaAPIController {
-    
+    public ResponseEntity<List<Turma>> listaTurma (){
+        var  listaTurma = new ArrayList<Turma>();
+
+        return
+            new ResponseEntity<List<Turma>>(listaTurma, HttpStatus.OK);
+    }
     
 }
