a077918f0805a2e5f467924bec71bbaf69018c04
Daslan Mateus <70038813+Daslan-Mateus@users.noreply.github.com>
2023-11-07 00:53:27 +0000

    add controller

---
+++ microservturma/src/main/java/br/edu/univille/microservturma/controller/TurmaAPIController.java
@@ -0,0 +1,10 @@
+package br.edu.univille.microservturma.controller;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller 
+@RequestMapping("api/v1/turma")
+public class TurmaAPIController {
+    
+    
+}
