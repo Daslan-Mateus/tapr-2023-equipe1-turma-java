44a5bacab9356076bc089fff7dc8eb66a3fabf39
Daslan Mateus <70038813+Daslan-Mateus@users.noreply.github.com>
2023-11-13 21:57:16 +0000

    Controller

--- microservturma/src/main/java/br/edu/univille/microservturma/controller/TurmaAPIController.java
+++ microservturma/src/main/java/br/edu/univille/microservturma/controller/TurmaAPIController.java
@@ -4,16 +4,20 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import br.edu.univille.microservturma.entity.Turma;
+import br.edu.univille.microservturma.service.TurmaService;
 
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
 @Controller 
 @RequestMapping("api/v1/turma")
 public class TurmaAPIController {
+    @Autowired
+    private TurmaService service;
     @GetMapping
     public ResponseEntity<List<Turma>> listaTurma (){
         var  listaTurma = new ArrayList<Turma>();
--- microservturma/src/main/java/br/edu/univille/microservturma/service/impl/TurmaServiceImpl.java
+++ microservturma/src/main/java/br/edu/univille/microservturma/service/impl/TurmaServiceImpl.java
@@ -1,8 +1,29 @@
 package br.edu.univille.microservturma.service.impl;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import br.edu.univille.microservturma.repository.TurmaRepository;
+import br.edu.univille.microservturma.service.TurmaService;
+
 @Service
-public class TurmaServiceImpl {
-    
+public class TurmaServiceImpl implements TurmaService {
+    @Autowired
+    private TurmaRepository repository;
+
+    @Override
+    public List<Turma> getAll() {
+        var iterador = repository.findAll();
+        List<Turma>listTurmas = new ArrayList <>();
+        
+        iterador.forEach(listTurmas :: add);
+
+        while (iterador.iterator().hasNext()) {
+            var umItem = iterador.iterator().next();
+            listTurmas.add(umItem);
+            
+        }
+
+
+        return listTurmas;
 }
